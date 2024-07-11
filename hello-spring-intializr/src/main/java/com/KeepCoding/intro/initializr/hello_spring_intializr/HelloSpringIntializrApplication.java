30e0e973af1138f714d5e054e2324a7a84efeeda
Montse Morata <sinaimh@gmail.com>
2024-07-11 14:32:17 +0200

    "primer commit"

---
+++ hello-spring-intializr/src/main/java/com/KeepCoding/intro/initializr/hello_spring_intializr/HelloSpringIntializrApplication.java
@@ -0,0 +1,13 @@
+package com.KeepCoding.intro.initializr.hello_spring_intializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringIntializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringIntializrApplication.class, args);
+	}
+
+}
---
+++ hello-spring-intializr/src/test/java/com/KeepCoding/intro/initializr/hello_spring_intializr/HelloSpringIntializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KeepCoding.intro.initializr.hello_spring_intializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringIntializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
